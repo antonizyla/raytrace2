7591880a724e6d4e7f98badb63ac8405d3386c53
Antoni Zyla <zyla.antoni@gmail.com>
2023-01-18 14:24:06 +0000

    update .gitigore

---
+++ src/ray.hpp
@@ -0,0 +1,8 @@
+//
+// Created by antoni on 1/18/23.
+//
+
+#ifndef RAYTRACE2_RAY_HPP
+#define RAYTRACE2_RAY_HPP
+
+#endif //RAYTRACE2_RAY_HPP
---
+++ src/sphere.hpp
@@ -0,0 +1,8 @@
+//
+// Created by antoni on 1/18/23.
+//
+
+#ifndef RAYTRACE2_SPHERE_HPP
+#define RAYTRACE2_SPHERE_HPP
+
+#endif //RAYTRACE2_SPHERE_HPP
