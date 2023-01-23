4913029fcbe1cc5273384b35525ba15e017dba31
Antoni Zyla <zyla.antoni@gmail.com>
2023-01-23 22:47:12 +0000

    halfway to ui

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,14 @@
 
 #include "renderer.hpp"
 
+#include <GLFW/glfw3.h>
+
 int main() {
     renderer r(400, 90);
     r.render();
+
+    glfwInit();
+    GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
+
     return 0;
 }
